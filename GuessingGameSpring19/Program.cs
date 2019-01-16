80a51989fbce3e39afcfc16048ea2fe4ad7f15db
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-01-16 13:15:13 -0500

    First Iteration Complete

--- GuessingGameSpring19/Program.cs
+++ GuessingGameSpring19/Program.cs
@@ -6,8 +6,24 @@ namespace GuessingGameSpring19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Adios!");
+            Console.WriteLine("Please guess a number between 1 and 10.");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
+
+            // if they guess 7 
+            if (userGuess.Equals("7"))
+            {
+                // then they win
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                // otherwise they lose
+                Console.WriteLine("You lost.");
+
+
+
+            }
         }
     }
 }
