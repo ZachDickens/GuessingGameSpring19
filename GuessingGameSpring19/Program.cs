7a06f6547d3f94ca09438487c713ff5f3a7fadbc
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-01-17 21:36:32 -0500

    Finished Game

--- GuessingGameSpring19/Program.cs
+++ GuessingGameSpring19/Program.cs
@@ -6,45 +6,55 @@ namespace GuessingGameSpring19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please guess a number between 1 and 10, or enter 0 for help.");
-            
             string userGuess = Console.ReadLine();
-
+            Random rand = new Random();
             int numberOfGuesses = 3;
-
-            if (userGuess.Equals("0"))
-            {
-                Console.WriteLine("=====INSTRUCTIONS=====");
-                Console.WriteLine("Guess a number between 1 and 10.");
-                Console.WriteLine("Hit enter to submit your guess.");
-
-                userGuess = Console.ReadLine();
-            }
-
-            Console.WriteLine("You guessed: " + userGuess);
-
-            // if they guess 7 
-            if (userGuess.Equals("7"))
-            {
-                // then they win
-                Console.WriteLine("You win!");
-                System.Environment.Exit(1);
-            }
-            else if (userGuess.Equals("-1"))
-            {
-                System.Environment.Exit(1);
-            }
-            else
-            {
-                numberOfGuesses = numberOfGuesses - 1;
-                userGuess = Console.ReadLine();
-                Console.Write("I am sorry.");
-                Console.WriteLine("That is incorrect. Please guess again.");
+            int correctAnswer = rand.Next(10);
             
-            }
-            Console.WriteLine("Please guess a number between 1 and 10, or enter 0 for help.");
-            userGuess = Console.ReadLine();
+            rand.Next(correctAnswer);
+            Console.WriteLine(correctAnswer);
+            do
+            {
+                Console.WriteLine("Please guess a number between 1 and 10, or enter 0 for help.");
+                userGuess = Console.ReadLine();
 
+                if (Int32.Parse(userGuess).Equals("0"))
+                {
+                    Console.WriteLine("=====INSTRUCTIONS=====");
+                    Console.WriteLine("Guess a number between 1 and 10.");
+                    Console.WriteLine("Hit enter to submit your guess.");
+
+                    userGuess = Console.ReadLine();
+                }
+
+                Console.WriteLine("You guessed: " + userGuess);
+
+                // if they guess 7 
+                if (Int32.Parse(userGuess).Equals(correctAnswer))
+                {
+                    // then they win
+                    Console.WriteLine("You win!");
+                    System.Environment.Exit(1);
+                }
+                else if (Int32.Parse(userGuess).Equals("-1"))
+                {
+                    System.Environment.Exit(1);
+                }
+                else
+                {
+                    if(correctAnswer > Int32.Parse(userGuess))   
+                    {
+                      Console.WriteLine("Your guess was too low.");
+                    } else
+                    {
+                        Console.WriteLine("Your guess was too high.");
+                    }
+                    numberOfGuesses = numberOfGuesses - 1;                    
+                    Console.WriteLine("That is incorrect. Please guess again.");
+                }
+                
+
+            } while (numberOfGuesses > 0);
 
         }
 
